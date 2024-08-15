6ced78fb68afc61a1c05797f63c0057f611ec708
SmartRightHere <thebarsik26@gmail.com>
2024-08-15 16:21:36 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from database import *
+from router import router as tasks_router
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print('database cleared')
+    await create_tables()
+    print('database created')
+    yield
+    print('turn off')
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
